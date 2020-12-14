4715cb00a99ed133153760b48bd02d9119f51499
Parikshith C R <rpari490@gmail.com>
2020-12-14 23:58:59 +0530

    initial UI

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="jumbotron mt-3">
+            <h1 className="display-4">Car Rental System</h1>
+            <p className="lead">This project is designed so as to be used by Car Rental Company
+        specializing  in renting cars to customers .</p>
+            <hr className="my-4" />
+            <p>The Car Rental System is being developed for customers so that they can  book their vehicles when they required .</p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+            </div>
+        </div>
+    )
+}
